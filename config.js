da2b94388be24911c5ecc0ee8b329aa3984e6881
Lampidis1 <129479209+Lampidis1@users.noreply.github.com>
2026-04-08 08:49:31 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: 'https://tlxzfircclsgpyafhhsu.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_5ig5kOUZn-UN5JOV5ubtpg_N1GXNQgS',
-  STORAGE_BUCKET_VISITAS: 'visitas-fotos'
-};
